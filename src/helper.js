84a2e14d35acb84da90e16b96124939cc9b11501
Adam Zygadlewicz <adam@zygadlewicz.com>
2017-05-09 13:55:05 +0200

    fix

--- src/helper.js
+++ src/helper.js
@@ -20,7 +20,7 @@ export function parseJSON(response)
 
 export function getJSON(url, callback, context)
 {
-	fetch(process.env.REACT_APP_API + url, {
+	fetch(url, {
       method: "GET",
       headers: {
         'Content-Type': 'application/json'
@@ -32,7 +32,7 @@ export function getJSON(url, callback, context)
 
 export function postJSON(url, data, callback)
 {
-	fetch(process.env.REACT_APP_API + url, {
+	fetch(url, {
       method: "POST",
       headers: {
         'Content-Type': 'application/json'
@@ -57,4 +57,3 @@ export function validatePhone(phone) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(phone);
 }
-
